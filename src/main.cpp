f6125af6f85b4f8557bb280af1580ba15436520e
auda-cz <55245600+auda-cz@users.noreply.github.com>
2024-04-29 17:03:11 -0400

    Add measurement averaging

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include "../lib/SHTC3.h"
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char** argv) {
+    int avgCount = 6;  // Default average count
+    float totalTemperature = 0.0, totalHumidity = 0.0;
+    if (argc > 1) {
+        avgCount = std::atoi(argv[1]);
+    }
+
+
     SHTC3 sensor(1); // Assuming I2C bus 1
-    auto [temperature, humidity] = sensor.getMeasurements();
-    std::cout << "Temperature: " << temperature << " C\n";
-    std::cout << "Humidity: " << humidity << "%\n";
+    for(int i=0;i < avgCount; i++ )
+    {
+        auto [temperature, humidity] = sensor.getMeasurements();
+        totalTemperature += temperature;
+        totalHumidity += humidity;
+        usleep(1000);
+    }
+    
+    std::cout << "Temperature: " << totalTemperature / avgCount << " C\n";
+    std::cout << "Humidity: " << totalHumidity / avgCount << "%\n";
     return 0;
 }
